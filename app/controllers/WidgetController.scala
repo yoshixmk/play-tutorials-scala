14437c3b7d95a31e008392d0e4b9df0f693f9f61
YoshihiroUeki <pirori67@gmail.com>
2019-07-31 23:25:14 +0900

    replace

--- app/controllers/WidgetController.scala
+++ app/controllers/WidgetController.scala
@@ -7,6 +7,16 @@ import play.api.data._
 import play.api.i18n._
 import play.api.mvc._
 
+/**
+ * The classic WidgetController using MessagesAbstractController.
+ *
+ * Instead of MessagesAbstractController, you can use the I18nSupport trait,
+ * which provides implicits that create a Messages instance from a request
+ * using implicit conversion.
+ *
+ * See https://www.playframework.com/documentation/2.7.x/ScalaForms#passing-messagesprovider-to-form-helpers
+ * for details.
+ */
 class WidgetController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
   import WidgetForm._
 
@@ -16,6 +26,9 @@ class WidgetController @Inject()(cc: MessagesControllerComponents) extends Messa
     Widget("Widget 3", 789)
   )
 
+  // The URL to the widget.  You can call this directly from the template, but it
+  // can be more convenient to leave the template completely stateless i.e. all
+  // of the "WidgetController" references are inside the .scala file.
   private val postUrl = routes.WidgetController.createWidget()
 
   def index = Action {
@@ -23,15 +36,21 @@ class WidgetController @Inject()(cc: MessagesControllerComponents) extends Messa
   }
 
   def listWidgets = Action { implicit request: MessagesRequest[AnyContent] =>
+    // Pass an unpopulated form to the template
     Ok(views.html.listWidgets(widgets.toSeq, form, postUrl))
   }
 
+  // This will be the action that handles our form post
   def createWidget = Action { implicit request: MessagesRequest[AnyContent] =>
     val errorFunction = { formWithErrors: Form[Data] =>
+      // This is the bad case, where the form had validation errors.
+      // Let's show the user the form again, with the errors highlighted.
+      // Note how we pass the form with errors to the template.
       BadRequest(views.html.listWidgets(widgets.toSeq, formWithErrors, postUrl))
     }
 
     val successFunction = { data: Data =>
+      // This is the good case, where the form was successfully parsed as a Data object.
       val widget = Widget(name = data.name, price = data.price)
       widgets.append(widget)
       Redirect(routes.WidgetController.listWidgets()).flashing("info" -> "Widget added!")
--- src/main/scala/models/Widget.scala
+++
@@ -1,3 +0,0 @@
-package models
-
-case class Widget(name: String, price: Int)
