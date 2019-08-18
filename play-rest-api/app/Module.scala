e7217eb3e1061e81bee67ec188ef7e775748271f
YoshihiroUeki <pirori67@gmail.com>
2019-08-19 00:22:17 +0900

    comment add.

--- play-rest-api/app/Module.scala
+++ play-rest-api/app/Module.scala
@@ -8,6 +8,9 @@ import v1.post._
 /**
   * Sets up custom components for Play.
   *
+  * If you call this module Module and place it in the root package,
+  * it will automatically be registered with Play.
+  *
   * https://www.playframework.com/documentation/latest/ScalaDependencyInjection
   */
 class Module(environment: Environment, configuration: Configuration)
