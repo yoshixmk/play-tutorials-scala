9aedab68010d511bd368d107df1c5c10ee250900
y.ueki <pirori67@gmail.com>
2019-11-10 22:54:04 +0900

    Intersection Types

--- samples/dotty-app/src/main/scala/Main.scala
+++ samples/dotty-app/src/main/scala/Main.scala
@@ -8,3 +8,31 @@ object Main {
   def msg = "I was compiled by dotty :)"
 
 }
+
+// == Intersection Types ==
+trait Resettable {
+  def reset(): this.type
+}
+trait Growable[T] {
+  def add(x: T): this.type
+}
+// 2つの型を持っていれば、エラーにならない
+def f(x: Resettable & Growable[String]) = {
+  x.reset()
+  x.add("first")
+}
+
+trait A {
+  def children: List[A]
+}
+trait B {
+  def children: List[B]
+}
+class C extends A with B {
+  def children: List[A & B] = ???
+}
+
+val x: A & B = new C
+val xChildren: List[A & B] = x.children
+// これはできない
+// val yChildren: List[C] = y.children
