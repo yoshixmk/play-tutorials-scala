64dd72f959725cd6b4b099546a46077efb0f11d7
YoshihiroUeki <pirori67@gmail.com>
2019-11-14 23:57:35 +0900

    ls

---
+++ app/controllers/TryScalaController.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.cache.Cache
+import play.api.Play.current
+import play.api.data._
+import play.api.data.format.Formats._
+import sys.process._
+import form.TryScalaForm
+
+import play.api.db._
+
+object TryScalaController extends Controller {
+
+  def exec = Action {
+    val form = Form(
+      mapping(
+        "command" -> of[String]
+      )(TryScalaForm.apply)(TryScalaForm.unapply)
+    )
+
+    val result = "ls -al" !
+    Ok(result)
+  }
+}
---
+++ app/form/TryScalaForm.scala
@@ -0,0 +1,5 @@
+package form
+
+case class TryScalaForm(command: String)
+
+
