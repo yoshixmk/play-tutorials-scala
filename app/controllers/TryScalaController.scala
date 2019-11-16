56f1cb8aca45627f69c5347fab234c70d2de5789
YoshihiroUeki <pirori67@gmail.com>
2019-11-16 23:14:20 +0900

    application fix(wip

--- app/controllers/TryScalaController.scala
+++ app/controllers/TryScalaController.scala
@@ -2,7 +2,6 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.cache.Cache
 import play.api.Play.current
 import play.api.data._
 import play.api.data.format.Formats._
